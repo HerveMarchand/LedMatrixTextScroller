47d4d701ac5ecef6deb4dbf6fc0538b4f9ba09b8
HerveMarchand <49374872+HerveMarchand@users.noreply.github.com>
2020-03-01 12:16:15 +0100

    Add files via upload

---
+++ example/sample_time/sample_time.ino
@@ -0,0 +1,65 @@
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include "matrixTextScroller.h"
+
+const char *ssid     = "YOUR_SSID";
+const char *password = "YOUR_PWD";
+
+
+int pinCLK = 10;
+int pinCS  = 11;
+int pinDIN = 12;
+
+MatrixTextScrollerClass myTestMatrix = MatrixTextScrollerClass(pinCLK, pinCS, pinDIN);
+
+const long utcOffsetInSeconds = 3600;
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+
+void setup(){
+  Serial.begin(115200);
+
+  myTestMatrix.setTextToDisplay("DATE");
+  myTestMatrix.setTextUpdatePeriod(30);
+  myTestMatrix.setIntensity(3);
+
+  WiFi.begin(ssid, password);
+
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+
+  Serial.println( "Connected" );
+
+  timeClient.begin();
+  
+  timeClient.update();
+}
+
+unsigned long lastNtpUpdate = 0;
+
+void loop() {
+
+  String dateStr;
+
+  // update ntp every hour
+  if(millis() - lastNtpUpdate > (60 * 60 * 1000) || lastNtpUpdate == 0) {
+    lastNtpUpdate = millis();
+    timeClient.update();
+  }
+
+  // Update date when text is no more displayed
+  if(false == myTestMatrix.getDisplayStatus()) {
+    // The text is no mode displayed
+    dateStr = timeClient.getFormattedTime();
+    myTestMatrix.setTextToDisplayWithLoopNumber(dateStr, 1);
+    myTestMatrix.startDisplay();
+  }
+  
+  myTestMatrix.refresh();
+
+}
